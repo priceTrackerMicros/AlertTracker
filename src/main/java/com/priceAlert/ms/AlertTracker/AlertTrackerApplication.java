d05a191f6142b0d397afc80c4818f7daf0489742
RenzoRP <renzo.riccipujol@gmail.com>
2024-12-02 10:39:53 +0100

    Initial Spring Boot setup

---
+++ src/main/java/com/priceAlert/ms/AlertTracker/AlertTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.priceAlert.ms.AlertTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlertTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/priceAlert/ms/AlertTracker/AlertTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.priceAlert.ms.AlertTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
