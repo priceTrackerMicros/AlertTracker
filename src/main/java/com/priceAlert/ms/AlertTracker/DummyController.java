a6091c883449e8e0cbc945ce861f14dfff50df23
MohamedAmarani <61777025+MohamedAmarani@users.noreply.github.com>
2024-12-04 21:22:59 +0100

    Update DummyController.java

--- src/main/java/com/priceAlert/ms/AlertTracker/DummyController.java
+++ src/main/java/com/priceAlert/ms/AlertTracker/DummyController.java
@@ -1,4 +1,4 @@
-package com.priceAlert.AlertTracker;
+package com.priceAlert.ms.AlertTracker;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
