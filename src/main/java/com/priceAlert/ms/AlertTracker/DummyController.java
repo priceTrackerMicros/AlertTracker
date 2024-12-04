861ec0ca2e0202483addc449f696ec353b503bdd
MohamedAmarani <61777025+MohamedAmarani@users.noreply.github.com>
2024-12-04 20:44:24 +0100

    Create DummyController.java

---
+++ src/main/java/com/priceAlert/ms/AlertTracker/DummyController.java
@@ -0,0 +1,14 @@
+package com.priceAlert.AlertTracker;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DummyController {
+
+    @GetMapping("/klk")
+    public String hello() {
+        return "klk";
+    }
+
+}
